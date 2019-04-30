d21b794db89130500d1780d1c8a33f7ccc4eecc9
unknown <lie4light@gmail.com>
2019-04-30 22:05:13 +0300

    Updates

---
+++ precache-manifest.2910801ac00ae49ac813ce086dd105e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d92f35f1efebc38103b8",
+    "url": "/goit-react-hw-02/static/js/runtime~main.577bbb3b.js"
+  },
+  {
+    "revision": "52443ff072a287483096",
+    "url": "/goit-react-hw-02/static/js/main.16beb9f8.chunk.js"
+  },
+  {
+    "revision": "f2134559fc09aef608e1",
+    "url": "/goit-react-hw-02/static/js/2.08acf79d.chunk.js"
+  },
+  {
+    "revision": "52443ff072a287483096",
+    "url": "/goit-react-hw-02/static/css/main.53453de1.chunk.css"
+  },
+  {
+    "revision": "0c32ca384e85cadefef9c5046a4483fa",
+    "url": "/goit-react-hw-02/index.html"
+  }
+];
